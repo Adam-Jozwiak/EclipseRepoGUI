9b27a9606e2c9d5db6bcd812f52bd4eb50967d94
unknown <adam.jozwiak94@wp.pl>
2018-02-19 23:11:49 +0100

    Signed-off-by: unknown <adam.jozwiak94@wp.pl>

---
+++ src/FirstClass.java
@@ -0,0 +1,17 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		String[] ciag = new String[3];
+		ciag[0]= "Siema";
+		ciag[1]= "Co tam?";
+		ciag[2]= "Mordoooo";
+		
+		for (int i=0; i<ciag.length; i++)
+		{
+			System.out.println(ciag[i]);
+		}
+				
+	}
+
+}
